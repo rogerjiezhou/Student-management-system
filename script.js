d52862af4515f9cd8684bedd4d0add9ca53f0212
Jie Zhou <zhoujie@my.bridgeport.edu>
2017-01-10 17:00:27 -0500

    Read json data

--- script.js
+++ script.js
@@ -1,49 +1,20 @@
 var storageSize;
+var limit;
 
 $(document).ready(function() {
   jQuery.ajax({
     "type": "GET",
     "url": "data.json",
     success: function(data) {
-      if(data) {
+      if (data) {
         if (typeof(Storage) !== "undefined") {
           var len = data.length;
           storageSize = len;
-          for(var i = 0; i < len; i++)
-            localStorage.setItem("array_"+i,JSON.stringify(data[i]))
+          for (var i = 0; i < len; i++)
+            localStorage.setItem("array_" + i, JSON.stringify(data[i]))
         }
-        updateList();
+        updateList(10);
       }
     }
   });
 });
-
-function updateList() {
-  html = '<table id="print">';
-        html += "<tr><th>Firstname</th>" +
-                "<th>Lastname</th>" +
-                "<th>Email</th>" +
-                "<th>Location</th>" +
-                "<th>Phone</th>" +
-                "<th>Current Class</th>" +
-                "<th>Address</th>" +
-                "<th>Option</th></tr>";
-        for (var i = 0; i < storageSize; i++) {
-          var current = JSON.parse(localStorage.getItem("array_"+i));
-          html += "<tr><td>" + current.firstname + "</td>" +
-                  "<td>" + current.lastname + "</td>" +
-                  "<td>" + current.email + "</td>" +
-                  "<td>" + current.location + "</td>" +
-                  "<td>" + current.phone + "</td>" +
-                  "<td>" + current.current_class + "</td>" +
-                  "<td>" + current.address.communication + "<br />" +
-                  current.address.permanent + "</td><tr>";
-        }
-        html += "<table>";
-        $("#list").html(html);
-}
-
-// function selectChange() {
-//     var x = document.getElementById("mySelect").value;
-//     document.getElementById("demo").innerHTML = "You selected: " + x;
-// }
